2982e7a5dd43a2969cf3fac701f20755cf1d2600
Simon Tucker <brotherlogic@gmail.com>
2023-08-18 11:45:52 -0700

    Working on ability to pull collection folders

---
+++ userfolders.go
@@ -0,0 +1,36 @@
+package discogs
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/brotherlogic/discogs/proto"
+)
+
+type GetFolderResponse struct {
+	Folders []Folder
+}
+
+type Folder struct {
+	Id   int
+	Name string
+}
+
+func (p *prodClient) GetUserFolders(ctx context.Context) ([]*pb.Folder, error) {
+	gfr := &GetFolderResponse{}
+	err := p.makeDiscogsRequest("GET", fmt.Sprintf("/users/%v/collection/folders", p.user.GetUsername()), "", cr)
+	if err != nil {
+		return nil, err
+	}
+
+	var folders []*pb.Folder
+	for _, folder := range gfr.Folders {
+		folders = append(folders, &pb.Folder{
+			Id:   int32(folder.Id),
+			Name: folder.Name,
+		})
+	}
+
+	return folders, nil
+
+}
