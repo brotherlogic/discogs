abfef4d8fa6b8fbbc82f15a308d562eae0b7db4b
Simon Tucker <brotherlogic@gmail.com>
2023-08-29 17:04:23 -0700

    Adds supporting test

--- testdata/build.sh
+++ testdata/build.sh
@@ -1,10 +1,14 @@
+mkdir -p marketplace/
+curl -X POST -H "Content-Type:application/json" -d '{"release_id": 27962688, "condition": "Mint (M)", "price": 100.23}'  --user-agent "GoDiscogsTestData" "https://api.discogs.com/marketplace/listings?token=$1"  | sed "s/$1/token/g" > marketplace/listings
+exit
+
 mkdir -p users/brotherlogic/collection/
 curl  --user-agent "GoDiscogsTestData" "https://api.discogs.com/users/brotherlogic/collection/folders?token=$1"  | sed "s/$1/token/g" > users/brotherlogic/collection/folders/FILE
 exit
 
 mkdir -p users/brotherlogic/collection/folders/0/releases/1163112/instances/19867414/fields/
 curl  --user-agent "GoDiscogsTestData" "https://api.discogs.com/users/brotherlogic/collection/fields?token=$1"  | sed "s/$1/token/g" > users/brotherlogic/collection/fields
-curl  -X POST -H "Content-Type:applicaion/json" -d '{"value": "Yes"}' --user-agent "GoDiscogsTestData" "https://api.discogs.com/users/BrotherLogic/collection/folders/0/releases/1163112/instances/19867414/fields/5?token=$1"  | sed "s/$1/token/g" > users/brotherlogic/collection/folders/0/releases/1163112/instances/19867414/fields/5
+curl  -X POST -H "Content-Type:applicaion/json" ---user-agent "GoDiscogsTestData" "https://api.discogs.com/users/BrotherLogic/collection/folders/0/releases/1163112/instances/19867414/fields/5?token=$1"  | sed "s/$1/token/g" > users/brotherlogic/collection/folders/0/releases/1163112/instances/19867414/fields/5
 mkdir -p users/brotherlogic/collection/folders/0/
 curl  --user-agent "GoDiscogsTestData" "https://api.discogs.com/users/brotherlogic/collection/folders/0/releases?token=$1&page=1&per_page=100" |  sed "s/$1/token/g" > users/brotherlogic/collection/folders/0/releases_page=1
 curl  --user-agent "GoDiscogsTestData" "https://api.discogs.com/users/brotherlogic/collection/folders/0/releases?token=$1&page=100&per_page=100" |  sed "s/$1/token/g" > users/brotherlogic/collection/folders/0/releases_page=100
